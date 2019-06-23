eecf6a8641f14a0375740e666e4e233246df5cb4
lxk000 <37549262+lxk000@users.noreply.github.com>
2019-06-23 22:40:37 +1000

    Update pim.ps1

--- pim.ps1
+++ pim.ps1
@@ -1,46 +1,39 @@
-function LaunchPad {
-    param(
-          [Parameter(Mandatory=$true)]
-		  [ValidateSet("Production", "Health DEV")]
-          [string] $Tenant,
-         )
-		Connect-AzAccount
-}
 
 function QuickPIM {
     param(
           [Parameter(Mandatory=$true)]
 		  [ValidateSet("Exchange Administrator", "User Administrator", "Global Administrator", "Password Administrator")]
-          [string] $Function,
+          [string] $Role,
 		  [string] $Ticket
          )
 		Connect-PimService
-		$assignPIM = Get-PrivilegedRoleAssignment | Where {$_.RoleName -eq $Function} | Select-Object RoleId
-		Enable-PrivilegedRoleAssignment -RoleAssignment $assignPIM -Duration 0.5 -TicketNumber $Ticket -Reason $Ticket -TicketSystem 'JIRA'
+		$assignPIM = Get-PrivilegedRoleAssignment | Where {$_.RoleName -eq $Role} | Select-Object RoleId
+		Enable-PrivilegedRoleAssignment -RoleAssignment $assignPIM -Duration 0.5 -TicketNumber $Ticket -Reason 'Platform Services' -TicketSystem 'JIRA'
 }
 
-$Menu = [ordered]@{
-
-  1 = 'Activate Privileged Role Assignment'
-
-  2 = 'Authenticate Azure Powershell Session'
-  }
-
-  $Result = $Menu | Out-GridView -PassThru  -Title 'Logged out'
-
-  Switch ($Result)  {
-
-  {$Result.Name -eq 1} 
- {
-iex (show-command QuickPIM -passthru)
-$Result = $Menu | Out-GridView -PassThru  -Title 'Privileged Roles Active'
+$Selection = [ordered]@{
+1 = 'Activate Privileged Role Assignment'
+2 = 'Authenticate Azure Powershell Session'
 }
 
-  {$Result.Name -eq 2} 
-  {
-iex (show-command LaunchPad -passthru)
-$Result = $Menu | Out-GridView -PassThru  -Title 'Powershell Session Logged in'
-  }
+	$Result = $Selection | Out-GridView -PassThru  -Title 'Login'
+
+	Switch ($Result)  {
+
+{	$Result.Name -eq 1} 
+{
+	iex (show-command QuickPIM -passthru)
+	$Result = $Menu | Out-GridView -PassThru  -Title 'Privileged Roles Active'
+}
+
+{	$Result.Name -eq 2} 
+{
+	Connect-AzAccount
+	$subscription = Get-AzSubscription | Out-GridView -PassThru -Title "Select Subscription"
+	Select-AzSubscription $subscription
+	#Launch a cli session
+}
 
 
 } 
+
